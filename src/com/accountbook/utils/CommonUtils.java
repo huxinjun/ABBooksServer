d24d837034e7a04690d90d4992f508d857351fe0
xinjun <hu12315005@126.com>
2018-01-18 16:21:34 +0800

    修复CommonUtils中计算时间的算法

--- src/com/accountbook/utils/CommonUtils.java
+++ src/com/accountbook/utils/CommonUtils.java
@@ -15,111 +15,57 @@ public class CommonUtils {
 	
 	
 	
-	@SuppressWarnings("deprecation")
-	public static void main(String[] args){
-		Date date = new Date();
-		date.setHours(10);
-		date.setMinutes(20);
-		System.out.println(getSinceTimeString(date));
-	}
-	
-	/**
-	 * 
-	 */
 	public static String getSinceTimeString(Date date){
 		long currTime=System.currentTimeMillis();
 		long time=date.getTime();
 		
-		int yearDis=getAllYear(currTime)-getAllYear(time);
-		int monthDis=getAllMonth(currTime)-getAllMonth(time);
-		int weekDis=getAllWeek(currTime)-getAllWeek(time);
-		int dayDis=getAllDay(currTime)-getAllDay(time);
-		int hourDis=getAllHour(currTime)-getAllHour(time);
 		int minuteDis=getAllMinute(currTime)-getAllMinute(time);
 		
-//		System.out.println(yearDis+"年\n"+monthDis+"月\n"+weekDis+"周\n"+dayDis+"日\n"+hourDis+"时\n"+minuteDis+"分\n--------------");
 		if(minuteDis==0)
 			return "刚刚";
-		if(minuteDis>0 && hourDis==0)
+		if(minuteDis>0 && minuteDis<60)
 			return minuteDis+"分钟前";
-		if(hourDis>0 && dayDis==0)
-			return hourDis+"小时前";
-		if(dayDis>0 && weekDis==0)
-			return dayDis+"天前";
-		if(weekDis>0 && monthDis==0)
-			return weekDis+"周前";
-		if(monthDis>0 && yearDis==0)
-			return monthDis+"月前";
-		if(yearDis>0)
-			return yearDis+"年前";
+		if(minuteDis>=60 && minuteDis<60*24)
+			return minuteDis/60+"小时前";
+		if(minuteDis>=60*24 && minuteDis<60*24*7)
+			return minuteDis/60/24+"天前";
+		if(minuteDis>=60*24*7 && minuteDis<60*24*7*30)
+			return minuteDis/60/24/7+"周前";
+		if(minuteDis>=60*24*7*30 && minuteDis<60*24*7*30*12)
+			return minuteDis/60/24/7/30+"月前";
+		if(minuteDis>=60*24*7*30*12)
+			return minuteDis/60/24/7/30/12+"年前";
 		return "";
 		
 	}
 	
-	/**
-	 * 
-	 */
 	public static String getSinceTimeString2(Date date){
-		
 		long currTime=System.currentTimeMillis();
 		long time=date.getTime();
 		
-		@SuppressWarnings("unused")
-		int yearDis=getAllYear(currTime)-getAllYear(time);
-		int monthDis=getAllMonth(currTime)-getAllMonth(time);
-		int weekDis=getAllWeek(currTime)-getAllWeek(time);
-		int dayDis=getAllDay(currTime)-getAllDay(time);
-		int hourDis=getAllHour(currTime)-getAllHour(time);
+		SimpleDateFormat format=new SimpleDateFormat("yyyy/MM/dd");
 		int minuteDis=getAllMinute(currTime)-getAllMinute(time);
 		
-		
-//		System.out.println("时间："+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date));
-//		System.out.println(yearDis+"年\n"+monthDis+"月\n"+weekDis+"周\n"+dayDis+"日\n"+hourDis+"时\n"+minuteDis+"分\n--------------");
-		
-		SimpleDateFormat format=new SimpleDateFormat("yyyy/MM/dd");
 		if(minuteDis==0)
 			return "刚刚";
-		if(minuteDis>0 && hourDis==0)
+		if(minuteDis>0 && minuteDis<60)
 			return minuteDis+"分钟前";
-		if(hourDis>0 && dayDis==0)
-			return hourDis+"小时前";
-		if(dayDis>0 && weekDis==0)
-			return dayDis+"天前";
-		if(weekDis>0 && monthDis==0)
-			return weekDis+"周前";
-		if(monthDis>0)
+		if(minuteDis>=60 && minuteDis<60*24)
+			return minuteDis/60+"小时前";
+		if(minuteDis>=60*24 && minuteDis<60*24*7)
+			return minuteDis/60/24+"天前";
+		if(minuteDis>=60*24*7 && minuteDis<60*24*7*30)
+			return minuteDis/60/24/7+"周前";
+		if(minuteDis>=60*24*7*30)
 			return format.format(time);
 		return "";
 		
 	}
 	
-	public static int getAllYear(long time){
-		return getAllMonth(time)/12;
-	}
-	
-	public static int getAllMonth(long time){
-//		Calendar c=Calendar.getInstance();
-//		c.setTime(new Date(time));
-//		int totalMonth=c.get(Calendar.YEAR)*12+c.get(Calendar.MONTH);
-//		return totalMonth;
-		return getAllDay(time)/30;
-	}
-	
-	public static int getAllWeek(long time){
-		return getAllDay(time)/7;
-	}
-	
-	public static int getAllDay(long time){
-		return getAllHour(time)/24;
-	}
-	
-	public static int getAllHour(long time){
-		return getAllMinute(time)/60;
-	}
-	
 	public static int getAllMinute(long time){
 		return (int) (time/1000/60);
 	}
+	
 	
 	
 	public static final int PAGE_DEFAULT_SIZE=10;
@@ -159,5 +105,6 @@ public class CommonUtils {
 		member.setMemberName(user.nickname);
 		return member;
 	}
+	
 	
 }
